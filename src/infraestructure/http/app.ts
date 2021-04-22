fbc6b943d176f2b95a27c77163510a7f1b2edf4d
Alejopdp <alejoscotti@gmail.com>
2021-04-22 20:17:21 +0200

    Config helmet

--- src/infraestructure/http/app.ts
+++ src/infraestructure/http/app.ts
@@ -6,10 +6,14 @@ import { v1Router } from "./api/v1";
 
 const app = express();
 
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(helmet());
 
 app.use("/api/v1", v1Router);
 
