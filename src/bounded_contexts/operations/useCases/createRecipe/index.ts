3b2bd7a1787d86122eb331706246701d8a45f7d8
Alejopdp <alejoscotti@gmail.com>
2021-06-11 18:53:33 +0200

    Mongoose repository for CreateRecipe

--- src/bounded_contexts/operations/useCases/createRecipe/index.ts
+++ src/bounded_contexts/operations/useCases/createRecipe/index.ts
@@ -1,9 +1,10 @@
 import { s3Service } from "../../application/storageService";
-import { mockRecipeRepository } from "../../infra/repositories/recipe";
+import { mockRecipeRepository, mongooseRecipeRepository } from "../../infra/repositories/recipe";
 import { mockWeekRepository } from "../../infra/repositories/week";
 import { recipeVariantCreator } from "../../services/recipeVariantCreator";
 import { CreateRecipe } from "./createRecipe";
 import { CreateRecipeController } from "./createRecipeController";
 
-export const createRecipe: CreateRecipe = new CreateRecipe(mockRecipeRepository, s3Service, mockWeekRepository, recipeVariantCreator);
+// export const createRecipe: CreateRecipe = new CreateRecipe(mockRecipeRepository, s3Service, mockWeekRepository, recipeVariantCreator);
+export const createRecipe: CreateRecipe = new CreateRecipe(mongooseRecipeRepository, s3Service, mockWeekRepository, recipeVariantCreator);
 export const createRecipeController: CreateRecipeController = new CreateRecipeController(createRecipe);
