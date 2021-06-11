42648cf9fcfb59fef6233c65c53826786ed5dc8a
Alejopdp <alejoscotti@gmail.com>
2021-06-11 10:19:10 +0200

    Remove nodemailerService

--- src/bounded_contexts/IAM/useCases/createUserAsAdmin/index.ts
+++ src/bounded_contexts/IAM/useCases/createUserAsAdmin/index.ts
@@ -1,16 +1,9 @@
-import { awsSesService, nodemailerService } from "../../../../shared/notificationService";
+import { awsSesService } from "../../../../shared/notificationService";
 import { jwtTokenService } from "../../application/tokenService";
 import { mockRoleRepository, mongooseRoleRepository } from "../../infra/repositories/role";
 import { mockUserRepository, mongooseUserRepository } from "../../infra/repositories/user";
 import { CreateUserAsAdmin } from "./createUserAsAdmin";
 import { CreateUserAsAdminController } from "./createUserAsAdminController";
-
-// export const createUserAsAdmin: CreateUserAsAdmin = new CreateUserAsAdmin(
-//     mockUserRepository,
-//     mockRoleRepository,
-//     nodemailerService,
-//     jwtTokenService
-// );
 
 export const createUserAsAdmin: CreateUserAsAdmin = new CreateUserAsAdmin(
     mongooseUserRepository,
