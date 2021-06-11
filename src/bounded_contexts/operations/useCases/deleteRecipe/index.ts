bc5fb1410ff198470d0bc3655d2fc73104c98512
Alejopdp <alejoscotti@gmail.com>
2021-06-11 19:25:21 +0200

    Added mongooseRepository

--- src/bounded_contexts/operations/useCases/deleteRecipe/index.ts
+++ src/bounded_contexts/operations/useCases/deleteRecipe/index.ts
@@ -1,6 +1,7 @@
-import { mockRecipeRepository } from "../../infra/repositories/recipe";
+import { mockRecipeRepository, mongooseRecipeRepository } from "../../infra/repositories/recipe";
 import { DeleteRecipe } from "./deleteRecipe";
 import { DeleteRecipeController } from "./deleteRecipeController";
 
-export const deleteRecipe: DeleteRecipe = new DeleteRecipe(mockRecipeRepository);
+// export const deleteRecipe: DeleteRecipe = new DeleteRecipe(mockRecipeRepository);
+export const deleteRecipe: DeleteRecipe = new DeleteRecipe(mongooseRecipeRepository);
 export const deleteRecipeController: DeleteRecipeController = new DeleteRecipeController(deleteRecipe);
