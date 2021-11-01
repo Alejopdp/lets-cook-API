67257508885f6e7da9ff5da705f48f9245351350
Alejopdp <alejoscotti@gmail.com>
2021-11-01 18:29:21 +0100

    feat: cancellation

---
+++ src/bounded_contexts/operations/useCases/exportCancellations/exportCancellations.ts
@@ -0,0 +1,57 @@
+import { ISubscriptionRepository } from "../../infra/repositories/subscription/ISubscriptionRepository";
+import { Subscription } from "../../domain/subscription/Subscription";
+import { ExportCancellationsDto } from "./exportCancellationsDto";
+import { CancellationExport, IExportService } from "../../application/exportService/IExportService";
+
+export class ExportCancellations {
+    private _subscriptionRepository: ISubscriptionRepository;
+    private _exportService: IExportService;
+
+    constructor(subscriptionRepository: ISubscriptionRepository, exportService: IExportService) {
+        this._subscriptionRepository = subscriptionRepository;
+        this._exportService = exportService;
+    }
+    public async execute(dto: ExportCancellationsDto): Promise<any> {
+        const cancelledSubscriptions: Subscription[] = await this.subscriptionRepository.findAllCancelledSubscriptions();
+        const exportRows: CancellationExport[] = [];
+
+        for (let subscription of cancelledSubscriptions) {
+            exportRows.push({
+                customerId: subscription.customer.id.value,
+                customerEmail: subscription.customer.email,
+                customerFirstName: subscription.customer.getPersonalInfo().name!,
+                customerLastName: subscription.customer.getPersonalInfo().lastName!,
+                subscriptionId: subscription.id.value,
+                status: subscription.state.humanTitle,
+                subscriptionCreatedAt: subscription.createdAt,
+                planTitle: subscription.plan.name,
+                planVariantTitle: subscription.getPlanVariantLabel(),
+                cancellationReason: subscription.cancellationReason?.title || "",
+                cancellationDate: subscription.cancellationReason?.date || "",
+                cancellationComment: "",
+                // numberOfActiveSubscriptions: 999,
+                // numberOfSubscriptions: 1,
+                // pastOrdersCount: 0,
+                // shopifyCustomerId: "",
+            });
+        }
+
+        this.exportService.exportCancellations(exportRows);
+    }
+
+    /**
+     * Getter subscriptionRepository
+     * @return {ISubscriptionRepository}
+     */
+    public get subscriptionRepository(): ISubscriptionRepository {
+        return this._subscriptionRepository;
+    }
+
+    /**
+     * Getter exportService
+     * @return {IExportService}
+     */
+    public get exportService(): IExportService {
+        return this._exportService;
+    }
+}
---
+++ src/bounded_contexts/operations/useCases/exportCancellations/exportCancellationsController.ts
@@ -0,0 +1,33 @@
+import { BaseController } from "../../../../core/infra/BaseController";
+import fs from "fs";
+import { ExportCancellations } from "./exportCancellations";
+import { ExportCancellationsDto } from "./exportCancellationsDto";
+
+export class ExportCancellationsController extends BaseController {
+    private _exportCancellations: ExportCancellations;
+
+    constructor(exportCancellations: ExportCancellations) {
+        super();
+        this._exportCancellations = exportCancellations;
+    }
+
+    protected async executeImpl(): Promise<any> {
+        try {
+            const dto: ExportCancellationsDto = {};
+
+            await this.exportCancellations.execute(dto);
+
+            return this.res.download("Cancelaciones.xlsx", (err) => fs.unlinkSync("Cancelaciones.xlsx"));
+        } catch (error) {
+            return this.fail(error);
+        }
+    }
+
+    /**
+     * Getter exportCancellations
+     * @return {ExportCancellations}
+     */
+    public get exportCancellations(): ExportCancellations {
+        return this._exportCancellations;
+    }
+}
