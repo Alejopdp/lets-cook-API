31cd38fec7ebc7c92ee6b6298b19d5913296b9b9
Alejopdp <alejoscotti@gmail.com>
2021-11-18 02:04:27 +0100

    feat: cors origin added

--- src/infraestructure/http/app.ts
+++ src/infraestructure/http/app.ts
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: true,
+        origin: process.env.CUSTOMER_ORIGIN_URL,
         credentials: true,
     })
 );
