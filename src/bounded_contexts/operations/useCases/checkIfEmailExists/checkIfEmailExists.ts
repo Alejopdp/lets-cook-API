ba441e626cf6d8617db24126338c9c5138c08164
Alejopdp <alejoscotti@gmail.com>
2021-12-10 10:14:30 -0300

    feat(iam): endpoint for checking if an email already exists

---
+++ src/bounded_contexts/operations/useCases/checkIfEmailExists/checkIfEmailExists.ts
@@ -0,0 +1,25 @@
+import { Customer } from "@src/bounded_contexts/operations/domain/customer/Customer";
+import { ICustomerRepository } from "@src/bounded_contexts/operations/infra/repositories/customer/ICustomerRepository";
+import { CheckIfEmailExistsDto } from "./checkIfEmailExistsDto";
+
+export class CheckIfEmailExists {
+    private _customerRepository: ICustomerRepository;
+
+    constructor(customerRepository: ICustomerRepository) {
+        this._customerRepository = customerRepository;
+    }
+
+    public async execute(dto: CheckIfEmailExistsDto): Promise<any> {
+        const customer: Customer | undefined = await this.customerRepository.findByEmail(dto.email);
+
+        return !!customer;
+    }
+
+    /**
+     * Getter customerRepository
+     * @return {ICustomerRepository}
+     */
+    public get customerRepository(): ICustomerRepository {
+        return this._customerRepository;
+    }
+}
