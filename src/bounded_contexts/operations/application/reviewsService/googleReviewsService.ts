1816834b858867156ce40f868fbad49bce19a062
Alejo Scotti <alejo@novolabs.xyz>
2022-10-18 17:32:31 +0200

    feat: integration finished

--- src/bounded_contexts/operations/application/reviewsService/googleReviewsService.ts
+++ src/bounded_contexts/operations/application/reviewsService/googleReviewsService.ts
@@ -27,49 +27,33 @@ export class GoogleReviewsService implements IReviewsService {
 
         try {
             let reviews: ReviewDto[] = []
-            const oauth2Client = new google.auth.OAuth2({ clientId: "859787193343-oqoqiqefal2s6vkc2sga1lhjgaskh855.apps.googleusercontent.com", clientSecret: "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", redirectUri: process.env.CUSTOMER_ORIGIN_URL, forceRefreshOnFailure: true })
-            await oauth2Client.getAccessToken(async (err, token, res) => {
-                console.log("Token err: ", err)
-                console.log("Token: ", token)
-                console.log("tokenRes: ", res)
-                const reviewRes = await axios.get(`https://mybusiness.googleapis.com/v4/accounts/104947658459106658401/locations/5728607255678643306/reviews`, { headers: { "Authorization": `Bearer ${token}` } })
+            const accessToken = await this.getAccessToken()
+            const reviewRes = await axios.get(`https://mybusiness.googleapis.com/v4/accounts/104947658459106658401/locations/5728607255678643306/reviews`, { headers: { "Authorization": `Bearer ${accessToken}` } })
+            if (reviewRes.status === 200) reviews = this.getMappedReviews(reviewRes.data.reviews)
 
-                if (reviewRes.status === 200) reviews = this.getMappedReviews(reviewRes.data.reviews)
-
-                return []
-            })
 
             return reviews
-            // const accessToken = await this.getAccessToken()
         } catch (error) {
             console.log("Error while trying to get all the reviews")
             console.error(error)
-            //@ts-ignore
-            console.log(error.response.data)
             return []
         }
     }
 
     private async getAccessToken(): Promise<string> {
-        const scopes = ["https://www.googleapis.com/auth/plus.business.manage"]
-        const oauth2Client = new google.auth.OAuth2({ clientId: "859787193343-oqoqiqefal2s6vkc2sga1lhjgaskh855.apps.googleusercontent.com", clientSecret: "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", redirectUri: process.env.CUSTOMER_ORIGIN_URL, forceRefreshOnFailure: true })
-        // const authorizationUrl = oauth2Client.generateAuthUrl({ access_type: "offline", scope: scopes, include_granted_scopes: true })
+        const oauth2Client = new google.auth.OAuth2({ clientId: "859787193343-oqoqiqefal2s6vkc2sga1lhjgaskh855.apps.googleusercontent.com", clientSecret: "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", redirectUri: process.env.CUSTOMER_ORIGIN_URL })
+        const refresh_token = "1//03ewfMsUOuIqVCgYIARAAGAMSNwF-L9IrWNvasWUzA85NlJhsbYOyUqL_NriBfthz_XR_oCt3_ZA3ggpLizr2CivOPIJ_a_7TRzQ" // TODO: This could expire
+        const scope = "https://www.googleapis.com/auth/business.manage openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email" // Scope returned from exchaging code for tokens
+
+        // 1st Step
+        // const authorizationUrl = oauth2Client.generateAuthUrl({ access_type: "offline", scope: scopes, include_granted_scopes: true, prompt: "consent" })
         // console.log("******************************Auth url: ", authorizationUrl)
-        // let { tokens } = await oauth2Client.getToken("4/0ARtbsJpdncLMTf_uUg8F9iDuApYjeRT684_tkNI4FSTbyeddfxsSquUSOwd_il8J2RtZiw")
-        // oauth2Client.setCredentials(tokens)
-        // console.log("TOKEN RES: ", tokens);
-        const res = await oauth2Client.getAccessToken((err, token, res) => {
-            console.log("Token err: ", err)
-            console.log("Token: ", token)
-            console.log("tokenRes: ", res)
-        })
+        // 2nd Step
+        // let { tokens } = await oauth2Client.getToken("4/0ARtbsJpbL6r6rQJWiaKSPS2b8W20HYzPl2WwEdnT3RSSqDX4JUmn2ujUugDcOzsyqqkhLQ")
+        oauth2Client.setCredentials({ refresh_token, scope })
+        const res = await oauth2Client.getAccessToken()
 
-        // return ""
         return res.token ?? ""
-        // const auth = new google.auth.GoogleAuth({ keyFile: `${__dirname}/../../../../../letscook-001-020bf42f3fa6.json`, scopes: ["https://www.googleapis.com/auth/plus.business.manage"] })
-        // const token = await auth.getAccessToken()
-
-        // return token ?? ""
     }
 
 
