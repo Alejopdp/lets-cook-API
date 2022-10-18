54cb31992474bc09bdeb21d7c0c0989d4dc8136a
Alejo Scotti <alejo@novolabs.xyz>
2022-10-18 10:27:12 +0200

    feat: WIP changed service account implementation to oauth

--- src/bounded_contexts/operations/application/reviewsService/googleReviewsService.ts
+++ src/bounded_contexts/operations/application/reviewsService/googleReviewsService.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { Locale } from "../../domain/locale/Locale";
 import { IReviewsService, ReviewDto } from "./IReviewsService";
-import { Auth } from "googleapis"
+import { google } from "googleapis"
+import client_secret from "../../../../../client_secret_859787193343-oqoqiqefal2s6vkc2sga1lhjgaskh855.apps.googleusercontent.com (1).json"
+import { access } from "fs";
 
 type GoogleStarRating = "ONE" | "TWO" | "THREE" | "FOUR" | "FIVE" | "STAR_RATING_UNSPECIFIED"
 
@@ -24,25 +26,52 @@ export class GoogleReviewsService implements IReviewsService {
     public async getAll(locale: Locale): Promise<ReviewDto[]> {
 
         try {
-            const accessToken = await this.getAccessToken()
-            const res = await axios.get(`https://mybusiness.googleapis.com/v4/accounts/104947658459106658401/locations/5728607255678643306/reviews`, { headers: { "Authorization": `Bearer ${accessToken}` } })
+            let reviews: ReviewDto[] = []
+            const oauth2Client = new google.auth.OAuth2({ clientId: "859787193343-oqoqiqefal2s6vkc2sga1lhjgaskh855.apps.googleusercontent.com", clientSecret: "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", redirectUri: process.env.CUSTOMER_ORIGIN_URL, forceRefreshOnFailure: true })
+            await oauth2Client.getAccessToken(async (err, token, res) => {
+                console.log("Token err: ", err)
+                console.log("Token: ", token)
+                console.log("tokenRes: ", res)
+                const reviewRes = await axios.get(`https://mybusiness.googleapis.com/v4/accounts/104947658459106658401/locations/5728607255678643306/reviews`, { headers: { "Authorization": `Bearer ${token}` } })
 
-            if (res.status === 200) return this.getMappedReviews(res.data.reviews)
+                if (reviewRes.status === 200) reviews = this.getMappedReviews(reviewRes.data.reviews)
 
-            return []
+                return []
+            })
+
+            return reviews
+            // const accessToken = await this.getAccessToken()
         } catch (error) {
             console.log("Error while trying to get all the reviews")
             console.error(error)
+            //@ts-ignore
+            console.log(error.response.data)
             return []
         }
     }
 
     private async getAccessToken(): Promise<string> {
-        const auth = new Auth.GoogleAuth({ keyFile: `${__dirname}/../../../../../letscook-001-020bf42f3fa6.json`, scopes: ["https://www.googleapis.com/auth/business.manage"] })
-        const token = await auth.getAccessToken()
+        const scopes = ["https://www.googleapis.com/auth/plus.business.manage"]
+        const oauth2Client = new google.auth.OAuth2({ clientId: "859787193343-oqoqiqefal2s6vkc2sga1lhjgaskh855.apps.googleusercontent.com", clientSecret: "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", redirectUri: process.env.CUSTOMER_ORIGIN_URL, forceRefreshOnFailure: true })
+        // const authorizationUrl = oauth2Client.generateAuthUrl({ access_type: "offline", scope: scopes, include_granted_scopes: true })
+        // console.log("******************************Auth url: ", authorizationUrl)
+        // let { tokens } = await oauth2Client.getToken("4/0ARtbsJpdncLMTf_uUg8F9iDuApYjeRT684_tkNI4FSTbyeddfxsSquUSOwd_il8J2RtZiw")
+        // oauth2Client.setCredentials(tokens)
+        // console.log("TOKEN RES: ", tokens);
+        const res = await oauth2Client.getAccessToken((err, token, res) => {
+            console.log("Token err: ", err)
+            console.log("Token: ", token)
+            console.log("tokenRes: ", res)
+        })
 
-        return token ?? ""
+        // return ""
+        return res.token ?? ""
+        // const auth = new google.auth.GoogleAuth({ keyFile: `${__dirname}/../../../../../letscook-001-020bf42f3fa6.json`, scopes: ["https://www.googleapis.com/auth/plus.business.manage"] })
+        // const token = await auth.getAccessToken()
+
+        // return token ?? ""
     }
+
 
     private getMappedReviews(googleReviews: GoogleReview[]): ReviewDto[] {
         const reviews: ReviewDto[] = []
