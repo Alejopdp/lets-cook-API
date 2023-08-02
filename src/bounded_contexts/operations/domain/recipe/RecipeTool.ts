9be7d035aaec223f6f2683422d809674abac1af2
Alejo Scotti <alejo@novolabs.xyz>
2023-08-02 12:27:45 +0200

    feat: repice tools and translations

--- src/bounded_contexts/operations/domain/recipe/RecipeTool.ts
+++ src/bounded_contexts/operations/domain/recipe/RecipeTool.ts
@@ -1,57 +1,577 @@
 export const RecipeTools: { [key: string]: { [locale: string]: string } } = {
-    "Cuchillo": {
-        en: "Knife",
-        ca: "Ganivet",
-        es: "Cuchillo"
+    "Bandeja de horno": {
+        "en": "Oven tray",
+        "ca": "Safata de forn",
+        "es": "Bandeja de horno"
     },
-    "Tabla de cortar": {
-        en: "Cutting board",
-        ca: "Taula de tallar",
-        es: "Tabla de cortar"
+    "Batidora": {
+        "en": "Blender",
+        "ca": "Batedora",
+        "es": "Batidora"
     },
-    "Olla": {
-        en: "Cooking pot",
-        ca: "Olla",
-        es: "Olla"
+    "Batidora (opcional)": {
+        "en": "Blender (optional)",
+        "ca": "Batedora (opcional)",
+        "es": "Batidora (opcional)"
     },
-    "Sartén": {
-        en: "Skillet",
-        ca: "Paella",
-        es: "Sartén"
-    },
-    "Wok": {
-        en: "Wok",
-        ca: "Wok",
-        es: "Wok"
+    "Batidora o minipimer": {
+        "en": "Blender or hand blender",
+        "ca": "Batedora o minimpimer",
+        "es": "Batidora o minipimer"
     },
     "Bol": {
-        en: "Bowl",
-        ca: "Bol",
-        es: "Bol"
+        "en": "Bowl",
+        "ca": "Bol",
+        "es": "Bol"
     },
-    "Recipiente de horno": {
-        en: "Furnace container",
-        ca: "Recipient de forn",
-        es: "Recipiente de horno"
+    "Bol grande": {
+        "en": "Large bowl",
+        "ca": "Bol gran",
+        "es": "Bol grande"
     },
-    "Bandeja de horno": {
-        en: "Oven plate",
-        ca: "Safata de forn",
-        es: "Bandeja de horno"
+    "Bol grande x2": {
+        "en": "Large bowl x2",
+        "ca": "Bol gran x2",
+        "es": "Bol grande x2"
+    },
+    "Bol pequeño": {
+        "en": "Small bowl",
+        "ca": "Bol petit",
+        "es": "Bol pequeño"
+    },
+    "Bol pequeño x2": {
+        "en": "Small bowl x2",
+        "ca": "Bol petit x2",
+        "es": "Bol pequeño x2"
+    },
+    "Bol x2": {
+        "en": "Bowl x2",
+        "ca": "Bol x2",
+        "es": "Bol x2"
+    },
+    "Bol x3": {
+        "en": "Bowl x3",
+        "ca": "Bol x3",
+        "es": "Bol x3"
+    },
+    "Bol x4": {
+        "en": "Bowl x4",
+        "ca": "Bol x4",
+        "es": "Bol x4"
+    },
+    "Bol x5": {
+        "en": "Bowl x5",
+        "ca": "Bol x5",
+        "es": "Bol x5"
+    },
+    "Colador": {
+        "en": "Colander",
+        "ca": "Colador",
+        "es": "Colador"
+    },
+    "Colador (opcional)": {
+        "en": "Colander (optional)",
+        "ca": "Colador (opcional)",
+        "es": "Colador (opcional)"
+    },
+    "Cuchillo": {
+        "en": "Knife",
+        "ca": "Ganivet",
+        "es": "Cuchillo"
+    },
+    "Ensaladera": {
+        "en": "Salad bowl",
+        "ca": "Bol per servir",
+        "es": "Ensaladera"
+    },
+    "Ensaladera x2": {
+        "en": "Salad bowl x2",
+        "ca": "Bol per servir x2",
+        "es": "Ensaladera x2"
+    },
+    "Espumadera": {
+        "en": "Skimmer",
+        "ca": "Escumadera",
+        "es": "Espumadera"
+    },
+    "Espumadera (opcional)": {
+        "en": "Skimmer (optional)",
+        "ca": "Escumadera (opcional)",
+        "es": "Espumadera (opcional)"
     },
     "Exprimidor": {
-        en: "Squeezer",
-        ca: "Espremedora",
-        es: "Exprimidor"
+        "en": "Squeezer",
+        "ca": "Espremedora",
+        "es": "Exprimidor"
     },
-    "Rallador": {
-        en: "Grater",
-        ca: "Ratllador",
-        es: "Rallador"
+    "Exprimidor (opcional)": {
+        "en": "Squeezer (optional)",
+        "ca": "Espremedora (opcional)",
+        "es": "Exprimidor (opcional)"
+    },
+    "Fuente de horno": {
+        "en": "Baking dish",
+        "ca": "Font de forn",
+        "es": "Fuente de horno"
+    },
+    "Fuente de horno (opcional)": {
+        "en": "Baking dish (optional)",
+        "ca": "Safata pel forn (opcional)",
+        "es": "Fuente de horno (opcional)"
+    },
+    "Fuente de horno x2": {
+        "en": "Baking dish x2",
+        "ca": "Safata pel forn x2",
+        "es": "Fuente de horno x2"
+    },
+    "Fuente de horno x3": {
+        "en": "Baking dish x3",
+        "ca": "Safata pel forn x3",
+        "es": "Fuente de horno x3"
+    },
+    "Fuente de horno x4": {
+        "en": "Baking dish x4",
+        "ca": "Safata pel forn x4",
+        "es": "Fuente de horno x4"
+    },
+    "Fuente de horno x5": {
+        "en": "Baking dish x5",
+        "ca": "Safata pel forn x5",
+        "es": "Fuente de horno x5"
     },
     "Minipimer": {
-        en: "Minipimer",
-        ca: "Minipimer",
-        es: "Minipimer"
+        "en": "Hand blender",
+        "ca": "Minipimer",
+        "es": "Minipimer"
     },
+    "Minipimer (opcional)": {
+        "en": "Hand blender (optional)",
+        "ca": "Minipimer (opcional)",
+        "es": "Minipimer (opcional)"
+    },
+    "Minipimer o batidora": {
+        "en": "Hand blender or blender",
+        "ca": "Minipimer o batedora",
+        "es": "Minipimer o batidora"
+    },
+    "Minipimer o mortero": {
+        "en": "Hand blender or mortar",
+        "ca": "Minipimer o morter",
+        "es": "Minipimer o mortero"
+    },
+    "Molde": {
+        "en": "Mold",
+        "ca": "Motlle",
+        "es": "Molde"
+    },
+    "Molde (opcional)": {
+        "en": "Mold (optional)",
+        "ca": "Motlle (opcional)",
+        "es": "Molde (opcional)"
+    },
+    "Olla": {
+        "en": "Pot",
+        "ca": "Olla",
+        "es": "Olla"
+    },
+    "Olla con tapa": {
+        "en": "Pot with lid",
+        "ca": "Olla amb tapa",
+        "es": "Olla con tapa"
+    },
+    "Olla con tapa x2": {
+        "en": "Pot with lid x2",
+        "ca": "Olla amb tapa x2",
+        "es": "Olla con tapa x2"
+    },
+    "Olla grande": {
+        "en": "Large pot",
+        "ca": "Olla gran",
+        "es": "Olla grande"
+    },
+    "Olla grande x2": {
+        "en": "Large pot x2",
+        "ca": "Olla gran x2",
+        "es": "Olla grande x2"
+    },
+    "Olla o sartén": {
+        "en": "Pot or frying pan",
+        "ca": "Olla o paella",
+        "es": "Olla o sartén"
+    },
+    "Olla o sartén con tapa": {
+        "en": "Pot or frying pan with lid",
+        "ca": "Olla o paella amb tapa",
+        "es": "Olla o sartén con tapa"
+    },
+    "Olla o sartén grande": {
+        "en": "Pot or large frying pan",
+        "ca": "Olla o paella gran",
+        "es": "Olla o sartén grande"
+    },
+    "Olla o sartén grande con tapa": {
+        "en": "Pot or large frying pan with lid",
+        "ca": "Olla o paella gran amb tapa",
+        "es": "Olla o sartén grande con tapa"
+    },
+    "Olla o sartén pequeña con tapa": {
+        "en": "Pot or small frying pan with lid",
+        "ca": "Olla o paella petita amb tapa",
+        "es": "Olla o sartén pequeña con tapa"
+    },
+    "Olla pequeña": {
+        "en": "Small pot",
+        "ca": "Olla petita",
+        "es": "Olla pequeña"
+    },
+    "Olla pequeña x2": {
+        "en": "Small pot x2",
+        "ca": "Olla petita x2",
+        "es": "Olla pequeña x2"
+    },
+    "Olla x2": {
+        "en": "Pot x2",
+        "ca": "Olla x2",
+        "es": "Olla x2"
+    },
+    "Olla x3": {
+        "en": "Pot x3",
+        "ca": "Olla x3",
+        "es": "Olla x3"
+    },
+    "Olla x4": {
+        "en": "Pot x4",
+        "ca": "Olla x4",
+        "es": "Olla x4"
+    },
+    "Olla x5": {
+        "en": "Pot x5",
+        "ca": "Olla x5",
+        "es": "Olla x5"
+    },
+    "Palillos": {
+        "en": "Toothpicks",
+        "ca": "Escuradents",
+        "es": "Palillos"
+    },
+    "Palillos (opcional)": {
+        "en": "Toothpicks (optional)",
+        "ca": "Escuradents (opcional)",
+        "es": "Palillos (opcional)"
+    },
+    "Papel aluminio": {
+        "en": "Aluminum foil",
+        "ca": "Paper d'alumini",
+        "es": "Papel aluminio"
+    },
+    "Papel aluminio (opcional)": {
+        "en": "Aluminum foli (optional)",
+        "ca": "Paper d'alumini (opcional)",
+        "es": "Papel aluminio (opcional)"
+    },
+    "Papel vegetal": {
+        "en": "Parchment paper",
+        "ca": "Paper de forn",
+        "es": "Papel vegetal"
+    },
+    "Papel vegetal (opcional)": {
+        "en": "Parchment paper (optional)",
+        "ca": "Paper de forn (opcional)",
+        "es": "Papel vegetal (opcional)"
+    },
+    "Parrilla o sartén": {
+        "en": "Griddle or frying pan",
+        "ca": "Graella o paella",
+        "es": "Parrilla o sartén"
+    },
+    "Parrilla o sartén con tapa": {
+        "en": "Griddle or frying pan with lid",
+        "ca": "Graella o paella amb tapa",
+        "es": "Parrilla o sartén con tapa"
+    },
+    "Parrilla o sartén grande": {
+        "en": "Griddle or large frying pan",
+        "ca": "Graella o paella gran",
+        "es": "Parrilla o sartén grande"
+    },
+    "Parrilla o sartén grande con tapa": {
+        "en": "Griddle or large frying pan with lid",
+        "ca": "Graella o paella gran amb tapa",
+        "es": "Parrilla o sartén grande con tapa"
+    },
+    "Parrilla o sartén pequeña con tapa": {
+        "en": "Griddle or small frying pan with lid",
+        "ca": "Graella o paella petita amb tapa",
+        "es": "Parrilla o sartén pequeña con tapa"
+    },
+    "Pelador": {
+        "en": "Peeler",
+        "ca": "Pelador",
+        "es": "Pelador"
+    },
+    "Pelador (opcional)": {
+        "en": "Peeler (optional)",
+        "ca": "Pelador (opcional)",
+        "es": "Pelador (opcional)"
+    },
+    "Plancha o sartén": {
+        "en": "Griddle or frying pan",
+        "ca": "Planxa o paella",
+        "es": "Plancha o sartén"
+    },
+    "Plancha o sartén con tapa": {
+        "en": "Griddle or frying pan with lid",
+        "ca": "Planxa o paella amb tapa",
+        "es": "Plancha o sartén con tapa"
+    },
+    "Plancha o sartén grande": {
+        "en": "Griddle or large frying pan",
+        "ca": "Planxa o paella gran",
+        "es": "Plancha o sartén grande"
+    },
+    "Plancha o sartén grande con tapa": {
+        "en": "Griddle or large frying pan with lid",
+        "ca": "Planxa o paella gran amb tapa",
+        "es": "Plancha o sartén grande con tapa"
+    },
+    "Plancha o sartén pequeña con tapa": {
+        "en": "Griddle or small frying pan with lid",
+        "ca": "Planxa o paella petita amb tapa",
+        "es": "Plancha o sartén pequeña con tapa"
+    },
+    "Plato hondo": {
+        "en": "Soup plate",
+        "ca": "Plat fondo",
+        "es": "Plato hondo"
+    },
+    "Plato hondo x2": {
+        "en": "Soup plate x2",
+        "ca": "Plat fondo x2",
+        "es": "Plato hondo x2"
+    },
+    "Plato hondo x3": {
+        "en": "Soup plate x3",
+        "ca": "Plat fondo x3",
+        "es": "Plato hondo x3"
+    },
+    "Plato hondo x4": {
+        "en": "Soup plate x4",
+        "ca": "Plat fondo x4",
+        "es": "Plato hondo x4"
+    },
+    "Plato hondo x5": {
+        "en": "Soup plate x5",
+        "ca": "Plat fondo x5",
+        "es": "Plato hondo x5"
+    },
+    "Rallador": {
+        "en": "Grater",
+        "ca": "Ratlladora",
+        "es": "Rallador"
+    },
+    "Rallador (opcional)": {
+        "en": "Grater (optional)",
+        "ca": "Ratlladora (opcional)",
+        "es": "Rallador (opcional)"
+    },
+    "Sartén": {
+        "en": "Frying pan",
+        "ca": "Paella",
+        "es": "Sartén"
+    },
+    "Sartén con tapa": {
+        "en": "Frying pan with lid",
+        "ca": "Paella amb tapa",
+        "es": "Sartén con tapa"
+    },
+    "Sartén con tapa x2": {
+        "en": "Frying pan with lid",
+        "ca": "Paella amb tapa x2",
+        "es": "Sartén con tapa x2"
+    },
+    "Sartén grande": {
+        "en": "Large frying pan",
+        "ca": "Paella gran",
+        "es": "Sartén grande"
+    },
+    "Sartén grande x2": {
+        "en": "Large frying pan x2",
+        "ca": "Paella gran x2",
+        "es": "Sartén grande x2"
+    },
+    "Sartén pequeña": {
+        "en": "Small frying pan",
+        "ca": "Paella petita",
+        "es": "Sartén pequeña"
+    },
+    "Sartén pequeña x2": {
+        "en": "Small frying pan x2",
+        "ca": "Paella petita x2",
+        "es": "Sartén pequeña x2"
+    },
+    "Sartén x2": {
+        "en": "Frying pan x2",
+        "ca": "Paella x2",
+        "es": "Sartén x2"
+    },
+    "Sartén x3": {
+        "en": "Frying pan x3",
+        "ca": "Paella x3",
+        "es": "Sartén x3"
+    },
+    "Sartén x4": {
+        "en": "Frying pan x4",
+        "ca": "Paella x4",
+        "es": "Sartén x4"
+    },
+    "Sarten x5": {
+        "en": "Frying pan x5",
+        "ca": "Paella x5",
+        "es": "Sarten x5"
+    },
+    "Tabla de cortar": {
+        "en": "Cutting board",
+        "ca": "Taula de tallar",
+        "es": "Tabla de cortar"
+    },
+    "Tapa": {
+        "en": "Lid",
+        "ca": "Tapa",
+        "es": "Tapa"
+    },
+    "Tapas": {
+        "en": "Lids",
+        "ca": "Tapes",
+        "es": "Tapas"
+    },
+    "Taza": {
+        "en": "Cup",
+        "ca": "Tassa",
+        "es": "Taza"
+    },
+    "Taza x2": {
+        "en": "Cup x2",
+        "ca": "Tassa x2",
+        "es": "Taza x2"
+    },
+    "Taza x3": {
+        "en": "Cup x3",
+        "ca": "Tassa x3",
+        "es": "Taza x3"
+    },
+    "Taza x4": {
+        "en": "Cup x4",
+        "ca": "Tassa x4",
+        "es": "Taza x4"
+    },
+    "Taza x5": {
+        "en": "Cup x5",
+        "ca": "Tassa x5",
+        "es": "Taza x5"
+    },
+    "Tazas": {
+        "en": "Cups",
+        "ca": "Tasses",
+        "es": "Tazas"
+    },
+    "Tijera": {
+        "en": "Scissors",
+        "ca": "Tisora",
+        "es": "Tijera"
+    },
+    "Tijera (opcional)": {
+        "en": "Scissors (optional)",
+        "ca": "Tisora (opcional)",
+        "es": "Tijera (opcional)"
+    },
+    "Vaso": {
+        "en": "Glass",
+        "ca": "Got",
+        "es": "Vaso"
+    },
+    "Vasos": {
+        "en": "Glasses",
+        "ca": "Gots",
+        "es": "Vasos"
+    },
+    "Wok": {
+        "en": "Wok",
+        "ca": "Wok",
+        "es": "Wok"
+    },
+    "Wok con tapa": {
+        "en": "Wok with lid",
+        "ca": "Wok amb tapa",
+        "es": "Wok con tapa"
+    },
+    "Wok con tapa x2": {
+        "en": "Wok with lid x2",
+        "ca": "Wok amb tapa x2",
+        "es": "Wok con tapa x2"
+    },
+    "Wok grande": {
+        "en": "Large wok",
+        "ca": "Wok gran",
+        "es": "Wok grande"
+    },
+    "Wok grande x2": {
+        "en": "Large wok x2",
+        "ca": "Wok gran x2",
+        "es": "Wok grande x2"
+    },
+    "Wok o sartén": {
+        "en": "Wok or frying pan",
+        "ca": "Wok o paella",
+        "es": "Wok o sartén"
+    },
+    "Wok o sartén con tapa": {
+        "en": "Wok or frying pan with lid",
+        "ca": "Wok o paella amb tapa",
+        "es": "Wok o sartén con tapa"
+    },
+    "Wok o sartén grande": {
+        "en": "Wok or large frying pan",
+        "ca": "Wok o paella gran",
+        "es": "Wok o sartén grande"
+    },
+    "Wok o sartén grande con tapa": {
+        "en": "Wok or large frying pan with lid",
+        "ca": "Wok o paella gran amb tapa",
+        "es": "Wok o sartén grande con tapa"
+    },
+    "Wok o sartén pequeña con tapa": {
+        "en": "Wok or small frying pan with lid",
+        "ca": "Wok o paella petita amb tapa",
+        "es": "Wok o sartén pequeña con tapa"
+    },
+    "Wok pequeño": {
+        "en": "Small wok",
+        "ca": "Wok petit",
+        "es": "Wok pequeño"
+    },
+    "Wok pequeño x2": {
+        "en": "Small wok x2",
+        "ca": "Wok petit x2",
+        "es": "Wok pequeño x2"
+    },
+    "Wok x2": {
+        "en": "Wok x2",
+        "ca": "Wok x2",
+        "es": "Wok x2"
+    },
+    "Wok x3": {
+        "en": "Wok x3",
+        "ca": "Wok x3",
+        "es": "Wok x3"
+    },
+    "Wok x4": {
+        "en": "Wok x4",
+        "ca": "Wok x4",
+        "es": "Wok x4"
+    },
+    "Wok x5": {
+        "en": "Wok x5",
+        "ca": "Wok x5",
+        "es": "Wok x5"
+    }
 }
