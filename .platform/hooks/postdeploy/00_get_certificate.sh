804f68617386e118946b0f35adcba755dac16bd4
Alejo Scotti <alejo@novolabs.xyz>
2023-09-12 17:57:44 +0200

    fix: certbot for staging

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d api.letscooknow.es --nginx --agree-tos --email alejo@novolabs.xyz
+sudo certbot -n -d staging.api.letscooknow.es --nginx --agree-tos --email alejo@novolabs.xyz
