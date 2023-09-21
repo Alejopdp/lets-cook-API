7cb662309070c2066651739ef5941cfee2e0d449
Alejo Scotti <alejo@novolabs.xyz>
2023-09-21 12:16:39 +0200

    fix: error handling of google reviews service, wallet job running every 10 min

--- src/shared/notificationService/index.ts
+++ src/shared/notificationService/index.ts
@@ -1,12 +1,4 @@
 import { INotificationService } from "./INotificationService";
-// import aws from "aws-sdk";
-// import { AwsSesService } from "./awsSesService";
 import { AwsV3SesService } from "./awsSesV3Service";
 
-// const ses = new aws.SES({
-//     region: "eu-west-3",
-//     credentials: { accessKeyId: process.env.SES_ACCES_KEY_ID!, secretAccessKey: process.env.SES_SECRET_ACCES_ID! },
-// });
-
-// export const awsSesService: INotificationService = new AwsSesService(ses);
 export const awsSesV3Service: INotificationService = new AwsV3SesService();
